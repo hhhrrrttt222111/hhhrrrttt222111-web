f2fc23544589b09c693a4ac08d05d8cb8354a4aa
Hemanth R <hemanthrt18@gmail.com>
2021-05-07 17:03:43 +0530

    👾

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 
 import { Landing } from './Pages'
 import { ThemeContext } from "./lib/context";
+import Loading from './Components/Loading/Loading';
 
 
 function App() {
 
   const [theme, setTheme] = useState('dark');
+  const [spinner, setSpinner] = useState(true);
+
+  useEffect(() => {
+      setTimeout(() => setSpinner(false), 2000)
+    }, []);
+
+    console.log("%cMade with 💚 by HRT", "color:#66ff66; font-size:30px");
+    console.log = console.warn = console.error = () => {};
+    console.error('Something bad happened.');
 
   return (
     <ThemeContext.Provider value={{theme, setTheme}}>
       <Router>
         <div className="app" data-theme={theme}>
-            <Landing theme={theme}/>
+        {spinner ? <>
+            <div className="spinner-container">
+                <Loading />
+            </div>
+            </> : <>
+              <Landing theme={theme}/>
+            </>
+        }
+            
         </div>
       </Router>
 
---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './Loading.css'
+
+function Loading() {
+
+    
+
+
+    return (
+        <div className="loadingContainer">
+            <div className="loading">
+                <span className="load load-1"></span>
+                <span className="load load-2"></span>
+                <span className="load load-3"></span>
+                <span className="load load-4"></span>
+                <span className="load load-5"></span>
+                <span className="load load-6"></span>
+                <span className="load load-7"></span>
+                <span className="load load-8"></span>
+            </div>
+        </div>
+
+    )
+}
+
+export default Loading
